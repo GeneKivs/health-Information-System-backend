049478ee6e952d09ddc0195af5db8986506c3e03
genekivs <genekivaze@gmail.com>
2026-02-17 17:52:55 +0300

    updated the sql file withan updated sql table creation code,added an application dev properties file and moved the database details in it which have been made environment variables in the local system

--- healthdb.session.sql
+++ healthdb.session.sql
@@ -1,8 +1,10 @@
+create database healthdb;
 CREATE TABLE healthPrograms(
-   health_Programid iNT NOT NULL,
-    health_Program_Name varchar(50),
+   health_Programid iNT NOT NULL auto_increment,
+   health_Program_Name varchar(50),
    description varchar(255),
-    createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
+   createdAt timestamp DEFAULT CURRENT_TIMESTAMP not null,
+   updatedAT timestamp default current_timestamp not null,
   PRIMARY KEY (health_Programid)
 );
 
@@ -13,7 +15,8 @@ CREATE TABLE clients(
     last_Name VARCHAR(50),
     age INT,
     phone_Number VARCHAR(15),
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	createdAt timestamp DEFAULT CURRENT_TIMESTAMP not null,
+	updatedAT timestamp default current_timestamp not null,
     PRIMARY KEY (clientid)
 
 );
@@ -22,8 +25,10 @@ create table enrollments(
 enrollmentid bigint not null auto_increment,
 clientid int not null,
 health_Programid int not null,
-enrollment_date date,
+createdAt timestamp DEFAULT CURRENT_TIMESTAMP not null,
+updatedAT timestamp default current_timestamp not null,
 primary key (enrollmentid),
 foreign key (clientid) references clients(clientid),
 foreign key (health_Programid) references healthPrograms(health_Programid)
+
 );
